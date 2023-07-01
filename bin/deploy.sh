230976485e5282fef08a316d2829a5c1a77b5205
itshkins <itshkins@gmail.com>
2023-07-01 14:02:39 +0300

    Fixes deploy script again

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,8 @@
 COMMIT_MESSAGE="${1?:error - provide commit message}"
 PROJECT_DIR="$(realpath "$(dirname "$0")/..")"
 
-npm version patch -f \
+npm test \
+  && npm version patch -f \
   && git add . \
   && git commit -m "$COMMIT_MESSAGE" \
   && git push \
