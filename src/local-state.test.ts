8193271f4dc0b7392acf8027fb9bd5247de50a96
itshkins <itshkins@gmail.com>
2023-07-01 14:10:19 +0300

    Small refactoring

--- src/local-state.test.ts
+++ src/local-state.test.ts
@@ -1,13 +1,15 @@
 import {describe, it, expect} from '@jest/globals'
+
 import {newLocalState} from './local-state'
 
 describe(`local state module`, () => {
   it(`writes and reads storage`, () => {
+    const DEFAULT_STATE = Object.freeze({})
+
     const localState = newLocalState(`my-key`)
-    const defaultState = {}
-    expect(localState.getState(defaultState)).toBe(defaultState)
+    expect(localState.getState(DEFAULT_STATE)).toBe(DEFAULT_STATE)
     expect(localState.setState({value: []})).toBe(true)
-    expect(localState.getState(defaultState)).toEqual({value: []})
+    expect(localState.getState(DEFAULT_STATE)).toEqual({value: []})
   })
 })
 
