b46bf2e78fe750a254d104266f4943a04db94d24
itshkins <itshkins@gmail.com>
2023-07-01 14:00:10 +0300

    1.1.6

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+COMMIT_MESSAGE="${1?:error - provide commit message}"
 PROJECT_DIR="$(realpath "$(dirname "$0")/..")"
 
-cd "$PROJECT_DIR" && \
-  npm version patch \
-  && git add . \
-  && git commit -m "$1" \
+git add . \
+  && npm version patch \
+  && git add package.json \
+  && git commit -m "$COMMIT_MESSAGE" \
   && git push \
   && npm publish
